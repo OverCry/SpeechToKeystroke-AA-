a23c70d8bfd6f20d1c479beca7aca10cfaf7b482
Wong <wcho400@aucklanduni.ac.nz>
2023-11-26 20:42:14 +1300

    allow looping, make word found more flexible

--- main.py
+++ main.py
@@ -1,19 +1,37 @@
+# done in python 3.9
+# windows
 from pynput.keyboard import Key, Controller
 import keyboard
 import speech_recognition as sr
 import sys
+import winsound
 
 keyboardPresser = Controller()
 r = sr.Recognizer()
 
 STOP_LOOPING = False
+frequency = 2500  # Set Frequency To 2500 Hertz
+duration = 100  # Set Duration To 1000 ms == 1 second
+
 def triggerCommand(command):
-    if command == 'objection':
+    if 'objection' in command:
         keyboardPresser.press('a')
-    elif command == 'press':
+        return True
+    elif 'press' in command:
         keyboardPresser.press('a')
-    elif command == 'present':
+        return True
+    elif 'present' in command:
         keyboardPresser.press('e')
+        return True
+    elif 'option' in command:
+        keyboardPresser.press('e')
+        return True
+    elif 'record' in command:
+        keyboardPresser.press('e')
+        return True
+    else:
+        return False
+
 
 def listen():
     global STOP_LOOPING
@@ -21,28 +39,25 @@ def listen():
 
     while True:
         if keyboard.is_pressed('w'):  # f
-            print('detected w')
             while not STOP_LOOPING:
-                print(STOP_LOOPING)
+                # while True:
                 with sr.Microphone() as source:
+                    # know when the system is listening
+                    winsound.Beep(frequency, duration)
                     # read the audio data from the default microphone
-                    audio_data = r.record(source, duration=3)
-                    print("Recognizing...")
-                    # convert speech to text
+                    audio_data = r.record(source, duration=2)
                     try:
                         text = r.recognize_google(audio_data)
-                        print(text)
-                        if text == 'stop' or text == 'exit':
-                            print("Please exit")
+                        # check text recognized
+                        # print(text)
+                        if 'stop' in text or 'quit' in text:
                             sys.exit()
-                        triggerCommand(text)
-                        STOP_LOOPING = True
+                        res = triggerCommand(text)
+                        if res:
+                            STOP_LOOPING = True
                     except sr.UnknownValueError:
                         print('No Voice Detected')
+            STOP_LOOPING = False
 
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     listen()
-
