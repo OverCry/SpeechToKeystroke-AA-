e5608c41e60482ab4c7f317069f57d305d2df3a4
Wong <wcho400@aucklanduni.ac.nz>
2023-11-26 20:20:07 +1300

    Responds to keypresses

--- main.py
+++ main.py
@@ -1,16 +1,48 @@
-ytho# This is a sample Python script.
+from pynput.keyboard import Key, Controller
+import keyboard
+import speech_recognition as sr
+import sys
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+keyboardPresser = Controller()
+r = sr.Recognizer()
 
+STOP_LOOPING = False
+def triggerCommand(command):
+    if command == 'objection':
+        keyboardPresser.press('a')
+    elif command == 'press':
+        keyboardPresser.press('a')
+    elif command == 'present':
+        keyboardPresser.press('e')
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def listen():
+    global STOP_LOOPING
+    STOP_LOOPING = False
+
+    while True:
+        if keyboard.is_pressed('w'):  # f
+            print('detected w')
+            while not STOP_LOOPING:
+                print(STOP_LOOPING)
+                with sr.Microphone() as source:
+                    # read the audio data from the default microphone
+                    audio_data = r.record(source, duration=3)
+                    print("Recognizing...")
+                    # convert speech to text
+                    try:
+                        text = r.recognize_google(audio_data)
+                        print(text)
+                        if text == 'stop' or text == 'exit':
+                            print("Please exit")
+                            sys.exit()
+                        triggerCommand(text)
+                        STOP_LOOPING = True
+                    except sr.UnknownValueError:
+                        print('No Voice Detected')
+
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    listen()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
